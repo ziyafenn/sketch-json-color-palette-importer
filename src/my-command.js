9905f654e8aeaed9ee90bb09f7b9589f9b3c3dc1
Ziya <ziya.fenn@gmail.com>
2020-08-18 11:19:38 +0200

    create and apply style

--- src/my-command.js
+++ src/my-command.js
@@ -1,49 +1,48 @@
 /* eslint-disable prefer-destructuring */
 // eslint-disable-next-line import/no-unresolved
 import sketch from 'sketch'
+import {INITIAL_VALUE} from "./shared"
 
 const document = sketch.getSelectedDocument()
 const page = document.selectedPage
 const Artboard = sketch.Artboard
 const Shape = sketch.ShapePath
 const UI = sketch.UI
+const SharedStyle = sketch.SharedStyle
+
 
 const newArtboard = (name, y) =>
   new Artboard({
     parent: page,
     name,
     frame: { x: 0, y, width: 1000, height: 100 },
+    flowStartPoint: true
   })
 
-const colorShape = (parent, name, x, sharedStyle) =>
+const colorShape = (parent, name, x, color) =>
   new Shape({
     parent,
     name,
     frame: { x, y: 0, width: 100, height: 100 },
-    sharedStyle,
-    /*    style: {
-      fills: [style],
-    },
-    */
+     style: {
+      fills: [color],
+    }
   })
 
-const newSharedStyle = (name, style) =>
-  document.sharedLayerStyles.push({
+  const newSharedStyle = (name, style) => SharedStyle.fromStyle({
     name,
-    styleType: 'Layer',
-    style: {
-      fills: [style],
-    },
+    style: style.style,
+    document,
   })
 
-let rectX = 0
-let rectY = 0
+let layerX = 0
+let artboardY = 0
 
 export default function() {
   UI.getInputFromUser(
     'Paste color palette JSON',
     {
-      initialValue: 'Appleseed',
+      initialValue: INITIAL_VALUE,
       numberOfLines: 30,
     },
     (err, value) => {
@@ -53,19 +52,22 @@ export default function() {
       }
       const parsed = JSON.parse(value)
       for (const palette in parsed) {
-        const paletteArboard = newArtboard(palette, rectY)
+        const paletteArboard = newArtboard(palette, artboardY)
         for (const [label, hex] of Object.entries(parsed[palette])) {
-          const paletteStyle = newSharedStyle(`${palette}/${label}`, hex)
-          //     log(paletteStyle.id);
-          colorShape(paletteArboard, label, rectX, hex)
-          rectX += 100
+         const newColor = colorShape(paletteArboard, label, layerX, hex);
+         const newStyle = newSharedStyle(`${palette}/${label}`, newColor)
+         newColor.sharedStyleId = newStyle.id
+         layerX += 100
         }
-        rectY += 200
-        rectX = 0
+        artboardY += 200
+        layerX = 0
       }
+      
     }
   )
+ 
 }
 
-// TODO Create artboard,
-// TODO create layer styles with palette/label
+// TODO fix linter issues
+// TODO Check if artboard exists, add after
+// TODO Manifest, publish
